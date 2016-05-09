d10819a238e8daac6ee83de215ee41aa1622e1dd
yzz <810178955@qq.com>
2016-05-09 10:08:09 +0800

    -给仓库添加文件

---
+++ MusicPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicPlayer
+//
+//  Created by 杨状状 on 16/5/9.
+//  Copyright © 2016年 杨状状. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MusicPlayer/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MusicPlayer
+//
+//  Created by 杨状状 on 16/5/9.
+//  Copyright © 2016年 杨状状. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+@end
