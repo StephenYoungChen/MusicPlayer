f6d5c6058716dd6b0c1c7dabf87720500674cddd
yzz <810178955@qq.com>
2016-05-10 11:30:00 +0800

    -导航设置背景色

--- MusicPlayer/ViewController.m
+++ MusicPlayer/ViewController.m
@@ -16,6 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.navigationItem.title = @"播放";
+    
+    self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:124.0/255.0 green:34.0/255.0 blue:104.0/255.0 alpha:1.0];
+    self.view.backgroundColor = [UIColor colorWithRed:219.0/255.0 green:81/255.0 blue:133.0/255.0 alpha:1.0];
     
 }
 
